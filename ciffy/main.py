2e781241839b30866a978f2cde7c2ae7f20a2f8f
Hamish B <hmblair@stanford.edu>
2025-08-08 15:04:32 -0700

    Moved reduction code

--- ciffy/main.py
+++ ciffy/main.py
@@ -2,58 +2,14 @@ from __future__ import annotations
 import os
 from enum import Enum
 from .enum import Residue, RES_ABBREV, RibonucleicAcid, Element
-from typing import Union, Generator
-import numpy as np
-from _ciffy_c import _load
+from .reduction import Reduction, _Reduction, REDUCTIONS
+from typing import Generator
 import torch
-from torch_scatter import (
-    scatter_sum as t_scatter_sum,
-    scatter_mean as t_scatter_mean,
-    scatter_max as t_scatter_max,
-    scatter_min as t_scatter_min,
-)
+from _ciffy_c import _load
 from copy import copy
 
+
 UNKNOWN = "UNKNOWN"
-
-class Reduction(Enum):
-    NONE = 0
-    COLLATE = 1
-    MEAN = 2
-    SUM = 3
-    MIN = 4
-    MAX = 5
-
-def t_scatter_collate(
-    features: torch.Tensor,
-    indices: torch.Tensor,
-    dim: int,
-    dim_size: int,
-) -> list[torch.Tensor]:
-    """
-    Return a list of tensors, containing all values corresponding to each value
-    of the index.
-    """
-
-    return [
-        features[indices == ix]
-        for ix in range(indices.max() + 1)
-    ]
-
-REDUCTIONS = {
-    Reduction.NONE: lambda features, indices, dim, dim_size: features,
-    Reduction.COLLATE: t_scatter_collate,
-    Reduction.MEAN: t_scatter_mean,
-    Reduction.SUM: t_scatter_sum,
-    Reduction.MIN: t_scatter_min,
-    Reduction.MAX: t_scatter_max,
-}
-
-_Reduction = Union[
-    torch.Tensor,
-    tuple[torch.Tensor, torch.LongTensor],
-    list[torch.Tensor],
-]
 
 
 class Scale(Enum):
@@ -69,6 +25,7 @@ class Molecule(Enum):
     OTHER = 3
     MISSING = 4
 
+
 def _molecule_type(val: int) -> Molecule:
 
     if val == 0:
@@ -82,9 +39,11 @@ def _molecule_type(val: int) -> Molecule:
     if val == 4:
         return Molecule.MISSING
 
+
 def _all_equal(*values: int) -> bool:
 
     return len(set(values)) == 1
+
 
 class Polymer:
 
